8a1a21039e30d4a8d4564001abe17828e6165460
ChangLin Ma <138810036@qqcom>
2022-05-14 19:34:20 +0800

    Update admin.py

--- typeidea/blog/admin.py
+++ typeidea/blog/admin.py
@@ -12,12 +12,18 @@ from .models import Post, Category, Tag
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name','status','is_nav','created_time')
+    def post_count(self,obj):
+        return obj.post_set.count()
+
+    list_display = ('name','status','is_nav','created_time','post_count')
     fields = ('name','status','is_nav','owner')
 
     def save_model(self,request,obj,form,change):
         obj.owner = request.user
         return super(CategoryAdmin,self).save_model(request,obj,form,change)
+
+    post_count.short_description = '文章数量'
+
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
